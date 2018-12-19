5a7f61413629ebdf9d31cfc4cd410640a8934673
Rodrigo Esteves <contact@rodrigoesteves.fr>
2018-12-19 13:03:29 +0100

    Mise à jour 2: Rodrigo

--- includes/header.php
+++ includes/header.php
@@ -7,25 +7,17 @@
 
   <div class="collapse navbar-collapse">
     <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Accueil</a>
+      </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="#">Société</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="#">Produits</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Contact</a>
       </li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
