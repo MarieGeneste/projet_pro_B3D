33cc2a0086ff13a912be543f79bdf19550361a88
ykazarinov <44573415+ykazarinov@users.noreply.github.com>
2018-12-21 12:54:44 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,61 @@
-<div class="footer">
-    <div class="container">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident ex, esse expedita eveniet debitis quasi sapiente, vitae explicabo quidem quos nostrum! Animi repellat laboriosam non quasi maxime blanditiis recusandae ipsum.
-    </div>
+<div class="obrez">
+<footer>
+</footer>
 </div>
+
+
+<div class="cont1">
+<div class="container">
+
+
+          <div class="row">
+
+            <div class="col-md-12 wo_marg">
+              
+              <div class="panel-white">
+              <div class="header"><i class="far fa-envelope"></i> Contact</div>
+              <div class="content">
+               
+
+                 <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                      <div class="contact_cont">
+                        <h2>Adresse</h2>
+                        <div class="contact_text">22 rue Hsesdca des swdew <br> 19000 Tulle</div>
+                      </div>
+
+                      <div class="contact_cont">
+                        <h2>Horaires</h2>
+                        <div class="contact_text">Lundi - Vendredi <br> 09:00 - 12:00 / 13:00 - 17:00</div>
+                      </div>
+                    </div>
+
+
+                    <div class="col-md-3 col-sm-6">
+                      <div class="contact_cont">
+                        <h2>Telephone</h2>
+                        <div class="contact_text">fix: +33.751.40.33.21 <br> portable: +33.632.43.23.12</div>
+                      </div>
+
+                      <div class="contact_cont">
+                        <h2>Courreil</h2>
+                        <div class="contact_text">info@b3d.fr</div>
+                      </div>
+                    </div>
+
+
+
+
+                    <div class="col-md-6  col-sm-12">
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2808.309593022266!2d1.7470402651390722!3d45.26175217909908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s22+rue+Hsesdca+des+swdew++19000+Tulle!5e0!3m2!1sfr!2sfr!4v1545333206179" width="100%" height="200" frameborder="0" style="border:1" allowfullscreen></iframe>
+                    </div>
+                  </div>
+
+                
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
